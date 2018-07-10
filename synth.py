10e7d0e58dbc1d908f1e72d5268429d4b590b581
Jonathan Lui <jonathanlui@google.com>
2018-07-10 13:06:27 -0700

    chore(build): use `npm ci` instead of `npm install`

--- synth.py
+++ synth.py
@@ -27,19 +27,13 @@ gapic = gcp.GAPICGenerator()
 versions = ['v2', 'v2beta1']
 for version in versions:
     library = gapic.node_library('dialogflow', version)
-    s.copy(library / 'protos')
-    s.copy(library / 'src' / version)
-    s.copy(library / 'test')
+
+s.copy(library, excludes=['package.json', 'README.md'])
 
 '''
 Node.js specific cleanup
 '''
-# Repo Cleanup/Setup
-subprocess.run(['npm', 'install'])
+subprocess.run(['npm', 'ci'])
 
-# Generates scaffolding, enters contributors names
-subprocess.run(['npm', 'run', 'generate-scaffolding'])
-
-# prettify and lint
 subprocess.run(['npm', 'run', 'prettier'])
 subprocess.run(['npm', 'run', 'lint'])
