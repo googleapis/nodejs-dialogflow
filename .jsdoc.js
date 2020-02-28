e0ec6e53045b753627a89f7939789445f65e0887
summerji <summerji@google.com>
2020-02-27 16:05:19 -0800

    chore: update jsdoc - protos and double quote

--- .jsdoc.js
+++ .jsdoc.js
@@ -42,7 +42,7 @@ module.exports = {
     systemName: '@google-cloud/dialogflow',
     theme: 'lumen',
     default: {
-      "outputSourceFiles": false
+      outputSourceFiles: false
     }
   },
   markdown: {
