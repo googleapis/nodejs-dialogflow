732cbdeb779d2ae65832d89c9a0d3c1ae3879873
Your Name <you@example.com>
2020-03-13 12:10:05 -0700

    remove dialogflow_list_knowledge_base region tag and code

--- samples/detect.v2beta1.js
+++ samples/detect.v2beta1.js
@@ -49,33 +49,6 @@ async function createKnowledgeBase(projectId, displayName) {
   // [END dialogflow_create_knowledge_base]
 }
 
-async function listKnowledgeBases(projectId) {
-  // [START dialogflow_list_knowledge_base]
-  // Imports the Dialogflow client library
-  const dialogflow = require('dialogflow').v2beta1;
-
-  // Instantiate a DialogFlow KnowledgeBasesClient.
-  const client = new dialogflow.KnowledgeBasesClient({
-    projectPath: projectId,
-  });
-
-  /**
-   * TODO(developer): Uncomment the following lines before running the sample.
-   */
-  // const projectId = 'ID of GCP project associated with your Dialogflow agent';
-
-  const formattedParent = client.projectPath(projectId);
-
-  const [resources] = await client.listKnowledgeBases({
-    parent: formattedParent,
-  });
-  resources.forEach(r => {
-    console.log(`displayName: ${r.displayName}`);
-    console.log(`name: ${r.name}`);
-  });
-  // [END dialogflow_list_knowledge_base]
-}
-
 async function createDocument(
   projectId,
   knowledgeBaseFullName,
