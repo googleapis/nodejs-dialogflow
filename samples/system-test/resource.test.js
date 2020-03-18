4ef446a54ef4e2520b1bbd295626d6d0076abbf7
Noah Negrey <nnegrey@users.noreply.github.com>
2020-03-18 16:05:44 -0600

    Update resource.test.js

--- samples/system-test/resource.test.js
+++ samples/system-test/resource.test.js
@@ -35,83 +35,6 @@ describe('resources', () => {
   let entityTypeId;
   let intentId;
 
-  it('should create a context', async () => {
-    const output = exec(
-      `${cmd} create-context -s ${sessionId} -c ${contextName} -l 3`
-    );
-    assert.include(output, sessionId);
-    assert.include(output, contextName);
-  });
-
-  it('should list contexts', async () => {
-    const output = exec(`${cmd} list-contexts -s ${sessionId}`);
-    assert.include(output, sessionId);
-    assert.include(output, contextName);
-    assert.include(output, '3');
-  });
-
-  it('should delete a context', async () => {
-    let output = exec(
-      `${cmd} delete-context -s ${sessionId} -c ${contextName}`
-    );
-    assert.include(output, sessionId);
-    assert.include(output, contextName);
-
-    output = exec(`${cmd} list-contexts -s ${sessionId}`);
-    assert.notInclude(output, sessionId);
-    assert.notInclude(output, contextName);
-  });
-
-  it('should create an entity type and entity', async () => {
-    const output = exec(
-      `${cmd} create-entity-type -d ${displayName} -k KIND_MAP`
-    );
-    assert.include(output, 'entityTypes');
-    entityTypeId = output.split(' ')[1].split('/')[4];
-  });
-
-  it('should List the Entity Type', async () => {
-    const output = exec(`${cmd} list-entity-types`);
-    assert.include(output, displayName);
-    assert.include(output, entityTypeId);
-  });
-
-  it('should Create an Entity for the Entity Type', async () => {
-    exec(
-      `${cmd} create-entity -e ${entityTypeId} -v ${entityName} -s ${synonym1} -s ${synonym2}`
-    );
-  });
-
-  it('should List the Entity', async () => {
-    const output = exec(`${cmd} list-entities -e ${entityTypeId}`);
-    assert.include(output, entityName);
-    assert.include(output, synonym1);
-    assert.include(output, synonym2);
-  });
-
-  it('should Delete the Entity', async () => {
-    let output = exec(
-      `${cmd} delete-entity -e ${entityTypeId} -v ${entityName}`
-    );
-    assert.include(output, entityName);
-
-    // Verify the Entity is Deleted
-    output = exec(`${cmd} list-entities -e ${entityTypeId}`);
-    assert.notInclude(output, entityName);
-    assert.notInclude(output, synonym1);
-    assert.notInclude(output, synonym2);
-  });
-
-  it('should Delete the Entity Type', async () => {
-    let output = exec(`${cmd} delete-entity-type -e ${entityTypeId}`);
-    assert.include(output, entityTypeId);
-
-    // Verify the Entity Type is Deleted
-    output = exec(`${cmd} list-entity-types`);
-    assert.notInclude(output, displayName);
-    assert.notInclude(output, entityTypeId);
-  });
-
   it('should create an intent', async () => {
     const output = exec(
       `${cmd} create-intent -d ${displayName} -t ${phrase1} -t ${phrase2} -m ${message1} -m ${message2}`
@@ -142,12 +65,6 @@ describe('resources', () => {
     entityTypeId = output.split(' ')[1].split('/')[4];
   });
 
-  it('should List the Entity Type', async () => {
-    const output = exec(`${cmd} list-entity-types`);
-    assert.include(output, displayName);
-    assert.include(output, entityTypeId);
-  });
-
   it('should Create a Session Entity Type', async () => {
     const output = exec(
       `${cmd} create-session-entity-type -s ${sessionId} -e ${synonym1} -e ${synonym2} -d ${displayName} -o ENTITY_OVERRIDE_MODE_OVERRIDE`
@@ -176,15 +93,5 @@ describe('resources', () => {
     assert.notInclude(output, sessionId);
     assert.notInclude(output, displayName);
     assert.notInclude(output, '2');
-  });
-
-  it('should Delete the Entity Type', async () => {
-    let output = exec(`${cmd} delete-entity-type -e ${entityTypeId}`);
-    assert.include(output, entityTypeId);
-
-    // Verify the Entity Type is Deleted
-    output = exec(`${cmd} list-entity-types`);
-    assert.notInclude(output, displayName);
-    assert.notInclude(output, entityTypeId);
   });
 });
