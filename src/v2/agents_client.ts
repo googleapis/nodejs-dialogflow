3b3c4c4c391012c7fb61b99742ecfc684e0f0bcd
yoshi-automation <yoshi-automation@google.com>
2020-03-21 04:20:48 -0700

    [CHANGE ME] Re-generated  to pick up changes in the API or client library generator.

--- src/v2/agents_client.ts
+++ src/v2/agents_client.ts
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -68,7 +68,12 @@ const version = require('../../../package.json').version;
  * @memberof v2
  */
 export class AgentsClient {
-  private _descriptors: Descriptors = {page: {}, stream: {}, longrunning: {}};
+  private _descriptors: Descriptors = {
+    page: {},
+    stream: {},
+    longrunning: {},
+    batching: {},
+  };
   private _innerApiCalls: {[name: string]: Function};
   private _pathTemplates: {[name: string]: gax.PathTemplate};
   private _terminated = false;
