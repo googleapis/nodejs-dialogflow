7a3913f3a34a4ad3deca70396c90ba872a0223d9
Noah Negrey <nnegrey@users.noreply.github.com>
2020-03-30 12:08:08 -0600

    Update detect.v2beta1.test.js

--- samples/system-test/detect.v2beta1.test.js
+++ samples/system-test/detect.v2beta1.test.js
@@ -21,58 +21,18 @@ const uuid = require('uuid/v4');
 
 const cmd = 'node detect.v2beta1.js';
 const testQuery = 'Where is my data stored?';
-const testKnowledgeBaseName = `${uuid().split('-')[0]}-TestKnowledgeBase`;
-const testDocName = 'TestDoc';
-const testDocumentPath = 'https://cloud.google.com/storage/docs/faq';
 
 const exec = cmd => execSync(cmd, {encoding: 'utf8'});
 
 describe('v2beta1 detection', () => {
-  let knowbaseFullName;
-  let knowbaseId;
-  let documentFullPath;
-
-  it('should create a knowledge base', () => {
-    // Check that the knowledge base does not yet exist
-    let output = exec(`${cmd} listKnowledgeBases`);
-    assert.notInclude(output, testKnowledgeBaseName);
-
-    // Creates a knowledge base
-    output = exec(`${cmd} createKnowledgeBase -k ${testKnowledgeBaseName}`);
-    assert.include(output, `displayName: ${testKnowledgeBaseName}`);
-
-    knowbaseFullName = output
-      .split('\n')[0]
-      .split(':')[1]
-      .trim();
-    knowbaseId = output
-      .split('\n')[0]
-      .split('knowledgeBases/')[1]
-      .trim();
-  });
-
-  it('should create a document', () => {
-    const output = exec(
-      `${cmd} createDocument -n "${knowbaseFullName}" -z "${testDocumentPath}" -m "${testDocName}"`
-    );
-    assert.include(output, 'Document created');
-  });
 
   it('should detect intent with a knowledge base', () => {
     const output = exec(
-      `${cmd} detectIntentKnowledge -q "${testQuery}" -n "${knowbaseId}"`
+      `${cmd} detectIntentKnowledge -q "${testQuery}" -n "OTAxMTY2MTA3MjkyNjUwNzAwOA"`
     );
     assert.include(output, 'Detected Intent:');
   });
 
-  it('should delete a document', () => {
-    const output = exec(`${cmd} deleteDocument -d ${documentFullPath}`);
-    assert.include(output, 'document deleted');
-  });
-
-  it('should delete the Knowledge Base', () => {
-    exec(`${cmd} deleteKnowledgeBase -n "${knowbaseFullName}"`);
-  });
 
   it('should detect Intent with Text to Speech Response', () => {
     const output = exec(
