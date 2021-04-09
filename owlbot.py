72ad496ac3ee74697e2a7312a4ca3be62a034f4b
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2021-04-09 13:43:56 -0400

    Add owlbot.py

--- owlbot.py
+++ owlbot.py
@@ -11,19 +11,47 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""This script is used to synthesize generated parts of this library."""
 
-import synthtool
+import os
+import synthtool as s
 import synthtool.gcp as gcp
 import synthtool.languages.node as node
+import json
 import logging
+from pathlib import Path
+
+# Load the default version defined in .repo-metadata.json.
+default_version = json.load(open(".repo-metadata.json", "rt"))["default_version"]
+staging = Path("owl-bot-staging")
+
+if staging.is_dir():
+  # Collect the subdirectories of the staging directory.
+  versions = [v.name for v in staging.iterdir() if v.is_dir()]
+  # Reorder the versions so the default version always comes last.
+  versions = [v for v in versions if v != default_version] + [default_version]
+  # Copy each version directory into the root.
+  for version in versions:
+      library = staging / version
+      _tracked_paths.add(library)
+      s_copy([library], excludes=["README.md", "package.json", "src/index.ts"])
+  # The staging directory should never be merged into the main branch.
+  shutil.rmtree(staging)
+else:
+  # Collect the subdirectories of the src directory.
+  src = Path("src")
+  versions = [v.name for v in src.iterdir() if v.is_dir()]
+  # Reorder the versions so the default version always comes last.
+  versions = [v for v in versions if v != default_version] + [default_version]
 
 # Copy common templates
 common_templates = gcp.CommonTemplates()
-templates = common_templates.node_library(
-    versions=node.detect_versions(),
-    default_version='v2'
-)
-synthtool.copy(templates, excludes=["README.md", "samples/README.md"])
+templates = common_templates.node_library(source_location='build/src', versions=versions, default_version=default_version)
+s.copy(templates, excludes=[
+  '.kokoro/presubmit/node12/system-test.cfg',
+  '.kokoro/release/docs-devsite.cfg',
+  '.kokoro/release/docs-devsite.sh',
+  'api-extractor.json'
+])
 
-# Lint, compile protos, etc.:
 node.postprocess_gapic_library_hermetic()
