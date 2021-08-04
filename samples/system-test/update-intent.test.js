a9fe228a156af99ef7d523895bc8865a75073e82
Gal Zahavi <galzahavi@google.com>
2021-08-04 11:39:15 -0700

    Added update intent sample

---
+++ samples/system-test/update-intent.test.js
@@ -0,0 +1,54 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+const {
+  AgentsClient,
+  IntentsClient,
+  protos,
+} = require('@google-cloud/dialogflow');
+const {assert} = require('chai');
+const {after, describe, it} = require('mocha');
+const execSync = require('child_process').execSync;
+const uuid = require('uuid');
+const projectId =
+  process.env.GCLOUD_PROJECT || process.env.GOOGLE_CLOUD_PROJECT;
+
+const exec = cmd => execSync(cmd, {encoding: 'utf8'});
+let intentID = '';
+
+describe('update intent', () => {
+  const cmd = 'node update-intent.js';
+  const displayName = `fake_display_name_${uuid.v4().split('-')[0]}`;
+
+  before('setup agent and get intent ID', () => {
+    // The path to identify the agent that owns the intents.
+
+    const intentClient = new IntentsClient();
+    const projectAgentPath = intentClient.projectAgentPath(projectId);
+
+    const intentRequest = {
+      parent: projectAgentPath,
+    };
+
+    const [response] = intentClient.listIntents(intentRequest);
+    intentID = response[0].name.split('/')[4];
+  });
+
+  it('should update an intent using fieldmasks', async () => {
+    const output = exec(`${cmd} ${projectId} ${intentID} ${displayName}`);
+    assert.include(output, displayName);
+  });
+
+});
