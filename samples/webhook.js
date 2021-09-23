5dc664eca8b67e111a86f1678379afbbefe13dd3
Gal Zahavi <galzahavi@google.com>
2021-09-23 14:50:08 -0700

    lint fix

--- samples/webhook.js
+++ samples/webhook.js
@@ -17,49 +17,49 @@
 // [START dialogflow_es_webhook]
 
 exports.handleWebhook = (request, response) => {
-    let tag = request.body.queryResult.intent.displayName;
-    console.log("HERE")
-    console.log(tag)
-    let jsonResponse = {};
-    if (tag === 'Default Welcome Intent') {
-        //fulfillment response to be sent to the agent if the request tag is equal to "welcome tag"
-        jsonResponse = {
-        fulfillment_messages: [
-            {
-                text: {
-                //fulfillment text response to be sent to the agent
-                text: ['Hello from a GCF Webhook'],
-                },
-            },
-            ],
-        };
-    } else if (tag === 'get-name') {
-        //fulfillment response to be sent to the agent if the request tag is equal to "welcome tag"
-        jsonResponse = {
-        fulfillment_messages: [
-            {
-                text: {
-                //fulfillment text response to be sent to the agent
-                text: ['My name is Flowhook'],
-                },
-            },
-            ],
-        };
-    } else {
-        jsonResponse = {
-        //fulfillment text response to be sent to the agent if there are no defined responses for the specified tag
-        fulfillment_messages: [
-            {
-                text: {
-                ////fulfillment text response to be sent to the agent
-                text: [
-                    `There are no fulfillment responses defined for "${tag}"" tag`,
-                ],
-                },
-            },
-            ],
-        };
-    }
-    response.send(jsonResponse);
+  const tag = request.body.queryResult.intent.displayName;
+  console.log('HERE');
+  console.log(tag);
+  let jsonResponse = {};
+  if (tag === 'Default Welcome Intent') {
+    //fulfillment response to be sent to the agent if the request tag is equal to "welcome tag"
+    jsonResponse = {
+      fulfillment_messages: [
+        {
+          text: {
+            //fulfillment text response to be sent to the agent
+            text: ['Hello from a GCF Webhook'],
+          },
+        },
+      ],
     };
+  } else if (tag === 'get-name') {
+    //fulfillment response to be sent to the agent if the request tag is equal to "welcome tag"
+    jsonResponse = {
+      fulfillment_messages: [
+        {
+          text: {
+            //fulfillment text response to be sent to the agent
+            text: ['My name is Flowhook'],
+          },
+        },
+      ],
+    };
+  } else {
+    jsonResponse = {
+      //fulfillment text response to be sent to the agent if there are no defined responses for the specified tag
+      fulfillment_messages: [
+        {
+          text: {
+            ////fulfillment text response to be sent to the agent
+            text: [
+              `There are no fulfillment responses defined for "${tag}"" tag`,
+            ],
+          },
+        },
+      ],
+    };
+  }
+  response.send(jsonResponse);
+};
 // [END dialogflow_es_webhook]
