1b7fb7e9b8c5f48a80e6e5e9b29af377749c2ebb
b-loved-dreamer <franklinsnunez@gmail.com>
2021-10-07 14:09:50 -0700

    docs(samples): adds new list training sample

---
+++ samples/system-test/listTrainingPhrases.test.js
@@ -0,0 +1,34 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const {assert} = require('chai');
+const {describe, before, it} = require('mocha');
+const execSync = require('child_process').execSync;
+const dialogflow = require('@google-cloud/dialogflow');
+const exec = cmd => execSync(cmd, {encoding: 'utf8'});
+
+const projectId =
+  process.env.GCLOUD_PROJECT || process.env.GOOGLE_CLOUD_PROJECT;
+const intentId = '7b5bd47e-6dd9-4b45-8624-565862bd2d85';
+
+describe('list training phrases', () => {
+  const cmd = 'node listTrainingPhrases.js';
+
+  it('should list training phrases in an intent', async () => {
+    const output = exec(`${cmd} ${projectId} ${intentId}`);
+    assert.isAtLeast(output, 9);
+  });
+});
