2b97a24846b8199cd93916c8cc408a677e5b5f94
Sofia Leon <sofialeon@google.com>
2022-11-11 00:40:53 -0800

    build: update README for deprecation notice and delete all files except samples

--- owlbot.py
+++ owlbot.py
@@ -1,16 +1,17 @@
 # Copyright 2022 Google LLC
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the License);
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-docker:
-  image: gcr.io/cloud-devrel-public-resources/owlbot-nodejs:latest
-  digest: sha256:5b05f26103855c3a15433141389c478d1d3fe088fb5d4e3217c4793f6b3f245e
+import synthtool.languages.node as node
+node.owlbot_main(templates_excludes=[
+'README.md'
+])
